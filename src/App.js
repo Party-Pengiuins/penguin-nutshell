be9cee63ce2555291372b3613b13ef448caa75da
ricky-bruner <alteredecho07@yahoo.com>
2018-08-31 10:48:38 -0500

    initial files, calm down

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import React from "react";
+
+export default class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+
+                <h1>Hi Penguins!</h1>
+            </React.Fragment>
+        )
+    }
+} 
