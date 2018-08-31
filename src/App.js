3e87a4194418c6f48112fdd7e6416af5ef465b5e
ricky-bruner <alteredecho07@yahoo.com>
2018-08-31 13:19:58 -0500

    file structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Component } from "react";
 import React from "react";
 
+
 export default class App extends Component {
     render() {
         return (
             <React.Fragment>
-
                 <h1>Hi Penguins!</h1>
             </React.Fragment>
         )
