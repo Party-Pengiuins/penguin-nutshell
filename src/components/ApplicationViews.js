11680c3ac498226e2f503f5a608040b842e7fecc
Klaus Hardt <klausheina@gmail.com>
2018-09-04 14:21:49 -0500

    appchnages

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 
-
 // import "./applicationViews.css";
 
 import { Route, Redirect } from "react-router-dom";
 import UserPage from "./UserPage";
+import NavBar from "./nav/NavBar";
+
+
 
 
 export default class ApplicationViews extends Component {
 
 
- 
 
-
-render() {
-    return (
-        <React.Fragment>
-            {
-                this.props.isAuthenticated() &&
-                <div className="viewArea">
-                    <Route exact path="/userpage" render={(props) => {
-                            return <UserPage />
-                        }} />
-                </div>
-            }
-            {
-                !this.props.isAuthenticated() &&
-                <Redirect to="/login" />
-            }
-        </React.Fragment>
-    )
-}
+    render() {
+        return (
+            <React.Fragment>
+                {
+                    this.props.isAuthenticated() &&
+                    <div className="viewArea">
+                        <NavBar />
+                        <Route exact path="/userpage" render={(props) => {
+                                return <UserPage />
+                            }} />
+                    </div>
+                }
+                {
+                    !this.props.isAuthenticated() &&
+                    <Redirect to="/login" />
+                }
+            </React.Fragment>
+        )
+    }
 }
