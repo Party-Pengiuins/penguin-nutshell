847b4ff544b0bcf998b1cb50dbe63ee9bfabdf1f
MissPeperr <madisonpeper@gmail.com>
2018-09-04 14:59:28 -0500

    deleted a state and componentDidMount from applicationViews

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 // import "./applicationViews.css";
 
 import { Route, Redirect } from "react-router-dom";
-import DataManager from "./modules/DataManager";
 import UserPage from "./UserPage";
 import NavBar from "./nav/NavBar";
 
@@ -11,22 +10,6 @@ import NavBar from "./nav/NavBar";
 
 
 export default class ApplicationViews extends Component {
-    state = {
-        articles: []
-    }
-
-    componentDidMount() {
-        const newState = {}
-
-        DataManager.getAll("articles")
-        .then(allArticles => newState.articles = allArticles)
-        .then(() => {
-            this.setState(newState)
-        })
-
-    }
-
-
 
     render() {
         return (
