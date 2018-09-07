cd332bb0fa5019a6448847327f0cda33d2177f86
MissPeperr <madisonpeper@gmail.com>
2018-09-07 14:52:56 -0500

    fixed articleCard error for the .includes

--- src/components/articles/ArticleCard.js
+++ src/components/articles/ArticleCard.js
@@ -8,36 +8,36 @@ library.add(faTrashAlt)
 
 const ArticleCard = ({article, deleteArticle}) => {
 
-    this.state = {
-        URL: article.URL
-    }
-    // An if statement for the user input:
-    // If the user's URL includes the 'https://', send the user to the URL
-    if(this.state.URL.includes("https://")){
+    // this.state = {
+    //     URL: article.URL
+    // }
+    // // An if statement for the user input:
+    // // If the user's URL includes the 'https://', send the user to the URL
+    // if(this.state.URL.includes("https://")){
+    //     return (
+    //         <div id={`article--${article.id}`} className="card article-card">
+    //             <div className="card-body">
+    //                 <h4 className="card-title">{article.title}</h4>
+    //                 <p>{article.description}</p>
+    //                 <a href={`${article.URL}`}>{article.title}</a>
+    //                     <div className="article-footer">
+    //                     <p>{article.date}</p>
+    //                         <Button id="delete-article-btn"
+    //                             onClick={() => deleteArticle("articles", article.id)}
+    //                             className="card-link"><FontAwesomeIcon icon="trash-alt" />
+    //                         </Button>
+    //                     </div>
+    //                 </div>
+    //         </div>
+    //     )
+    // // if the user's URL DOES NOT include the 'https://', add the 'https://' before the URL
+    // } else {
         return (
             <div id={`article--${article.id}`} className="card article-card">
                 <div className="card-body">
                     <h4 className="card-title">{article.title}</h4>
                     <p>{article.description}</p>
-                    <a href={`${article.URL}`}>{article.title}</a>
-                        <div className="article-footer">
-                        <p>{article.date}</p>
-                            <Button id="delete-article-btn"
-                                onClick={() => deleteArticle("articles", article.id)}
-                                className="card-link"><FontAwesomeIcon icon="trash-alt" />
-                            </Button>
-                        </div>
-                    </div>
-            </div>
-        )
-    // if the user's URL DOES NOT include the 'https://', add the 'https://' before the URL
-    } else {
-        return (
-            <div id={`article--${article.id}`} className="card article-card">
-                <div className="card-body">
-                    <h4 className="card-title">{article.title}</h4>
-                    <p>{article.description}</p>
-                    <a href={`https://${article.URL}`}>{article.title}</a>
+                    <a href={article.URL}>{article.title}</a>
                     <div className="article-footer">
                         <p>{article.date}</p>
                             <Button id="delete-article-btn"
@@ -48,7 +48,7 @@ const ArticleCard = ({article, deleteArticle}) => {
                     </div>
             </div>
         )
-    }
+    // }
 }
 
 ArticleCard.propTypes = {
